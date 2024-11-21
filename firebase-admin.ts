d4758e6e699105bee6e71cd4791eaf8259a453a7
Anurag123-cloud <anuragmaurya566@gmail.com>
2024-11-21 23:23:25 +0530

    done project

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,20 +1,22 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-
-
-import { getApp, getApps, initializeApp, cert,App } from "firebase-admin/app";
+import { getApp, getApps, initializeApp, cert, App } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-const serviceKey =require('./service_key.json')
 
 
-let app:App
 
- if (getApps().length===0) {
-    app=initializeApp({
-        credential: cert(serviceKey),
-      })
- } else {
-    app = getApp();
- }
+let app: App;
+
+if (getApps().length === 0) {
+  app = initializeApp({
+    credential: cert({
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"), // Handle newline characters
+      clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+    }),
+  });
+} else {
+  app = getApp();
+}
 
 const adminDb = getFirestore(app);
-export {adminDb,app as adminApp}
+
+export { adminDb, app as adminApp };
