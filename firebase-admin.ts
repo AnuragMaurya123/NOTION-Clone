a119a14550391530eeccc685cb9c60237ef57c5c
Anurag123-cloud <anuragmaurya566@gmail.com>
2024-11-22 01:17:58 +0530

    done project

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -10,7 +10,7 @@ if (getApps().length === 0) {
     credential: cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY, // Handle newline characters
-      clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+      clientEmail: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
     }),
   });
 } else {
